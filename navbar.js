a1988aef2afc20b63af95a419e3a2291896ce374
Andrew Crescencio <andrew_workstation@hotmail.com>
2022-04-02 00:56:42 -0300

    fix: navbar

--- navbar.js
+++ navbar.js
@@ -1,10 +1,28 @@
 export default class Navbar {
   constructor() {}
   navbarOn() {
+    this.navbarToggle();
+    this.navbarScrolling();
+  }
+  navbarToggle() {
     const navbarBtn = document.getElementById("navbar-btn");
     const navbarLinks = document.getElementById("navbar-links");
     navbarBtn.addEventListener("click", () => {
       navbarLinks.classList.toggle("active");
     });
+  }
+  navbarScrolling() {
+    const navbar = document.getElementById("navbar");
+    window.onscroll = function () {
+      const screenSize = window.screen.height / 20;
+      const item = document.querySelector("#navbar");
+      if (item) {
+        if (window.pageYOffset >= screenSize) {
+          item.classList.add("scrolling");
+        } else {
+          item.classList.remove("scrolling");
+        }
+      }
+    };
   }
 }

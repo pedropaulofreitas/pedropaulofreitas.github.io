6e28a39689048ced564f914041121dd4b3ba9254
Andrew Crescencio <andrew_workstation@hotmail.com>
2022-04-06 14:31:13 -0300

    fix: js folder

---
+++ js/app.js
@@ -0,0 +1,5 @@
+import "../scss/style.scss";
+import Navbar from "./navbar.js";
+const navbar = new Navbar();
+
+navbar.navbarOn();
---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+export default class Navbar {
+  constructor() {}
+  navbarOn() {
+    this.navbarToggle();
+    this.navbarScrolling();
+  }
+  navbarToggle() {
+    const navbarBtn = document.getElementById("navbar-btn");
+    const navbarLinks = document.getElementById("navbar-links");
+    const hamburger = document.getElementById("hamburger");
+
+    navbarBtn.addEventListener("click", () => {
+      navbarLinks.classList.toggle("active");
+      hamburger.classList.toggle("hamburger-open");
+    });
+  }
+  navbarScrolling() {
+    const navbar = document.getElementById("navbar");
+    window.onscroll = function () {
+      const screenSize = window.screen.height / 20;
+      const item = document.querySelector("#navbar");
+      if (item) {
+        if (window.pageYOffset >= screenSize) {
+          item.classList.add("scrolling");
+        } else {
+          item.classList.remove("scrolling");
+        }
+      }
+    };
+  }
+}

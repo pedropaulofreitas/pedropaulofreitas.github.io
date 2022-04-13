6e7d9aa538742d99dea2eb3cbb1630b911e6805e
Andrew Crescencio <andrew_workstation@hotmail.com>
2022-04-13 14:18:06 -0300

    fix: js && animate.css

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 export default class Navbar {
   constructor() {}
-  navbarOn() {
+  boot() {
     this.navbarToggle();
     this.navbarScrolling();
   }
--- public/js/app.js
+++
@@ -1,5 +0,0 @@
-import "../scss/style.scss";
-import Navbar from "./navbar.js";
-const navbar = new Navbar();
-
-navbar.navbarOn();

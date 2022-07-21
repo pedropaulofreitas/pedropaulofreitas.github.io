62d207108daef2ee2a6ba835e0249fb4c8adad35
Pedro Freitas <pedro.freitas@zesto.com.br>
2022-07-21 19:34:20 -0300

    fix

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:zeep/zeep.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:pedropaulofreitas/pedropaulofreitas.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
